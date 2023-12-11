fd55c663d19be11b5d0b1bcac879e2f0989cb595
tildejustin <spacepepper3.14@gmail.com>
2023-12-10 23:37:19 -0500

    fix case and comments

--- src/main/java/me/modmuss50/optifabric/mod/OptifineInjector.java
+++ src/main/java/me/modmuss50/optifabric/mod/OptifineInjector.java
@@ -2,7 +2,6 @@ package me.modmuss50.optifabric.mod;
 
 import com.chocohead.mm.api.ClassTinkerers;
 import me.modmuss50.optifabric.patcher.*;
-import net.fabricmc.loader.api.FabricLoader;
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.tree.*;
 
@@ -12,8 +11,8 @@ import java.util.function.Consumer;
 public class OptifineInjector {
     private static final List<String> patched = new ArrayList<>();
     ClassCache classCache;
-
-    // I have no idea why and how this works, if you know better please let me know
+    // i have no idea why and how this works, if you know better please let me know
+    @SuppressWarnings("CommentedOutCode")
     public final Consumer<ClassNode> transformer = target -> {
         if (patched.contains(target.name)) {
             System.out.println("Already patched" + target.name);
@@ -21,42 +20,43 @@ public class OptifineInjector {
         }
         patched.add(target.name);
 
-        // I cannot imagine this being very good at all
-        ClassNode source = this.getSourceClassNode(target);
+        // i cannot imagine this being very good at all
+        ClassNode source = getSourceClassNode(target);
 
         target.methods = source.methods;
         target.fields = source.fields;
         target.interfaces = source.interfaces;
         target.superName = source.superName;
 
-        // Classes should be read with frames expanded (as Mixin itself does it), in which case this should all be fine
+        // classes should be read with frames expanded (as mixin itself does it), in which case this should all be fine
         for (MethodNode methodNode : target.methods) {
             for (AbstractInsnNode insnNode : methodNode.instructions.toArray()) {
                 if (insnNode instanceof FrameNode) {
                     FrameNode frameNode = (FrameNode) insnNode;
                     if (frameNode.local == null)
-                        throw new IllegalStateException("Null locals in " + frameNode.type + " frame @ " + source.name + "#" + methodNode.name + methodNode.desc);
+                        throw new IllegalStateException("null locals in " + frameNode.type + " frame @ " + source.name + "#" + methodNode.name + methodNode.desc);
                 }
             }
         }
 
-        // Let's make every class we touch public
-        if (!FabricLoader.getInstance().isDevelopmentEnvironment()) {
-            target.access = modAccess(target.access);
-            target.methods.forEach(methodNode -> methodNode.access = modAccess(methodNode.access));
-            target.fields.forEach(fieldNode -> fieldNode.access = modAccess(fieldNode.access));
-        }
+        // let's make every class we touch public
+        // is this necessary? I don't think so
+        // if (!FabricLoader.getInstance().isDevelopmentEnvironment()) {
+        //     target.access = modAccess(target.access);
+        //     target.methods.forEach(methodNode -> methodNode.access = modAccess(methodNode.access));
+        //     target.fields.forEach(fieldNode -> fieldNode.access = modAccess(fieldNode.access));
+        // }
     };
 
     public OptifineInjector(ClassCache classCache) {
         this.classCache = classCache;
     }
 
+    @SuppressWarnings("unused")
     private static int modAccess(int access) {
         if ((access & 0x7) != Opcodes.ACC_PRIVATE)
             return (access & (~0x7)) | Opcodes.ACC_PUBLIC;
         return access;
-
     }
 
     public void setup() {
@@ -67,7 +67,7 @@ public class OptifineInjector {
         String name = classNode.name.replaceAll("\\.", "/") + ".class";
         byte[] bytes = classCache.getAndRemove(name);
         if (bytes == null)
-            throw new RuntimeException("Failed to find patched class for: " + name);
+            throw new RuntimeException("failed to find patched class for: " + name);
         return ASMUtils.readClassFromBytes(bytes);
     }
 }
--- src/main/java/me/modmuss50/optifabric/patcher/PatchSplitter.java
+++ src/main/java/me/modmuss50/optifabric/patcher/PatchSplitter.java
@@ -7,7 +7,7 @@ import org.zeroturnaround.zip.ZipUtil;
 import java.io.*;
 import java.util.jar.JarFile;
 
-//Pulls out the patched classes and saves into a classCache, and also creates an optifine jar without these classes
+// pulls out the patched classes and saves into a classCache, and also creates an optifine jar without these classes
 public class PatchSplitter {
     @SuppressWarnings("ResultOfMethodCallIgnored")
     public static ClassCache generateClassCache(File inputFile, File classCacheOutput, byte[] inputHash) throws IOException {
@@ -20,7 +20,7 @@ public class PatchSplitter {
         try (JarFile jarFile = new JarFile(inputFile)) {
             jarFile.stream().forEach(entry -> {
                 if ((entry.getName().startsWith("net/minecraft/") || entry.getName().startsWith("com/mojang/")) && entry.getName().endsWith(".class")) {
-                    // if the class does not exist we cannot replace it
+                    // if the class does not exist we cannot replace it, so we don't add it to the classCache and just let it get added to the classpath
                     if (Optifabric.class.getClassLoader().getResource(entry.getName()) == null) return;
                     try (InputStream inputStream = jarFile.getInputStream(entry)) {
                         String name = entry.getName();
@@ -36,11 +36,10 @@ public class PatchSplitter {
                 }
             });
         }
-
-        //Remove all the classes that are going to be patched in, we don't want theses on the classpath
+        // remove all the classes that are going to be patched in with replaceClass, we don't want these on the classpath
         ZipUtil.removeEntries(inputFile, classCache.getClasses().toArray(new String[0]));
 
-        System.out.println("Found " + classCache.getClasses().size() + " patched classes");
+        System.out.println("found " + classCache.getClasses().size() + " patched classes");
         classCache.save(classCacheOutput);
         return classCache;
     }
