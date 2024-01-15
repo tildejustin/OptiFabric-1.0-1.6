9387e66ab2e76ba2a426844aadfb54f27c3a7f53
tildejustin <spacepepper3.14@gmail.com>
2024-01-15 16:57:28 -0500

    fix quirky 1.7.2 ReflectorClass causing mixin issues, bump version

--- 1.6-1.7.10/src/main/java/me/modmuss50/optifabric/mixin/ReflectorClassMixin.java
+++ 1.6-1.7.10/src/main/java/me/modmuss50/optifabric/mixin/ReflectorClassMixin.java
@@ -4,23 +4,41 @@ import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.lang.reflect.Field;
+
 // suppresses some warnings in the logs
 @Pseudo
 @Mixin(targets = "ReflectorClass")
 public class ReflectorClassMixin {
-    @Shadow
-    private String targetClassName;
-
     @Shadow
     private boolean checked;
 
     @SuppressWarnings("UnresolvedMixinReference")
     @Inject(method = "getTargetClass", at = @At("HEAD"), remap = false)
     private void getTargetClass(CallbackInfoReturnable<Class<?>> infoReturnable) {
+        String targetClassName = this.getTargetClassName();
         if (!this.checked) { // only check the target if it hasn't been done yet
-            String name = this.targetClassName.replaceAll("/", ".");
+            String name = targetClassName.replaceAll("/", ".");
             if (name.startsWith("net.minecraft.launchwrapper") || name.startsWith("net.minecraftforge") || "optifine.OptiFineClassTransformer".equals(name)) {
                 this.checked = true;
+            }
+        }
+    }
+
+    @Unique
+    private String getTargetClassName() {
+        try {
+            Field targetClass = this.getClass().getDeclaredField("targetClassName");
+            targetClass.setAccessible(true);
+            return (String) targetClass.get(this);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            try {
+                // 1.7.2 behavior
+                Field targetClass = this.getClass().getDeclaredField("targetClassNames");
+                targetClass.setAccessible(true);
+                return ((String[]) targetClass.get(this))[0];
+            } catch (NoSuchFieldException | IllegalAccessException ex) {
+                throw new RuntimeException(ex);
             }
         }
     }
