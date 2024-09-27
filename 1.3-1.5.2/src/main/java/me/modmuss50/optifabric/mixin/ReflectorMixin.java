7161f392143cf4d38c4b7d85f25c043e8badcf17
Cat Core <arthur.koalas@free.fr>
2024-09-27 18:29:43 +0200

    Improve compatibility with Fabricated Legacy Forge on older Optifine versions

---
+++ 1.3-1.5.2/src/main/java/me/modmuss50/optifabric/mixin/ReflectorMixin.java
@@ -0,0 +1,25 @@
+package me.modmuss50.optifabric.mixin;
+
+import net.fabricmc.loader.api.FabricLoader;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.util.Map;
+
+@Pseudo
+@Mixin(targets = "Reflector")
+public class ReflectorMixin {
+    @SuppressWarnings("UnresolvedMixinReference")
+    @Redirect(method = "getClassNameMap", at = @At(value = "INVOKE", target = "Ljava/util/Map;put(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;", remap = false), remap = false)
+    private static Object fixClassNameForFabricatedForge(Map map, Object key, Object value) {
+        if (FabricLoader.getInstance().isModLoaded("fabricated-forge")) {
+            if (value instanceof String && !((String) value).contains(".")) {
+                value = "net.minecraft." + value;
+            }
+        }
+
+        return map.put(key, value);
+    }
+}
